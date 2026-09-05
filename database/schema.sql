af1339e98f03fe18846fcd30c52c819a713f26e5
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-09-05 12:04:25 -0300

    docs: add sql script for tables generation

---
+++ database/schema.sql
@@ -0,0 +1,114 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
+
+START TRANSACTION;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260904154429_MigracaoInicial') THEN
+
+    ALTER DATABASE CHARACTER SET utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260904154429_MigracaoInicial') THEN
+
+    CREATE TABLE `Produtos` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `Descricao` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
+        `Quantidade` int NOT NULL,
+        `Valor` decimal(12,2) NOT NULL,
+        `DataCadastro` datetime(6) NOT NULL,
+        `UsuarioCadastro` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+        CONSTRAINT `PK_Produtos` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260904154429_MigracaoInicial') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20260904154429_MigracaoInicial', '8.0.10');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
+START TRANSACTION;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260905010851_AdicionaUsuarios') THEN
+
+    CREATE TABLE `Usuarios` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `Nome` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+        `Email` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+        `SenhaHash` longtext CHARACTER SET utf8mb4 NOT NULL,
+        CONSTRAINT `PK_Usuarios` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260905010851_AdicionaUsuarios') THEN
+
+    CREATE UNIQUE INDEX `IX_Usuarios_Email` ON `Usuarios` (`Email`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260905010851_AdicionaUsuarios') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20260905010851_AdicionaUsuarios', '8.0.10');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
